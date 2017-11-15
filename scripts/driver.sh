54f53eb4ce3ae1d667bd07c30ece0866ea8135a8
Vignesh <vignesh.subramanian@berkeley.edu>
2017-11-15 01:38:17 +0000

    Bug fixes

--- scripts/driver.sh
+++ scripts/driver.sh
@@ -2,7 +2,7 @@
 # The first argument is a reservation number
 cd pcaps
 rsync -a --include '*srn*/' --include 'tr0.pcap' --include 'Inputs/' --include 'batch_input.json' --include 'match_conf.json' --exclude '*' njain@sc2-lz:/share/nas/uc-berkeley/RESERVATION-$1/ $1
-for f in $1/*/*; do editcap -D 3 $f ${f%.pcap}-edited.pcap; done
+for f in $1/*/*.pcap; do editcap -D 3 $f ${f%.pcap}-edited.pcap; done
 for f in $1/*/*-edited.pcap; do tshark -Y "ip.dsfield == 0x00" -r $f > ${f%.pcap}-leaky.txt; done
 for f in $1/*/*-edited.pcap; do tshark -Y "ip.dsfield == 0x80" -r $f > ${f%.pcap}-high.txt; done
 dest=$1
@@ -10,6 +10,6 @@ find $dest -type f | rename 's:(^|.*/)([^/]*)/([^/]*)$:${dest}/$2_$3:'
 for f in $dest/*.txt; do mv $f "${f/uc-berkeley-test-scenario-/}"; done
 cd ../
 arr=(pcaps/$dest/*-high.txt)
-for i in $(seq 0 ${#arr[@]}); do arr[@i]=${arr[$i]%high.txt}; done
+for i in $(seq 0 ${#arr[@]}); do arr[$i]=${arr[$i]%high.txt}; done
 mkdir images_$dest
-for f in "${arr[@]}"; do python pcaps2info.py 1 images_$dest ${f}leaky.txt ${f}high.txt; done
+for f in "${arr[@]}"; do python pcap2info.py 1 images_$dest ${f}leaky.txt ${f}high.txt; done
