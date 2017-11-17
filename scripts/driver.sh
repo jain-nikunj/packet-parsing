02d94a625129f1ecfbf44bef692da57acf0ee65a
Vignesh <vignesh.subramanian@berkeley.edu>
2017-11-17 03:00:49 +0000

    Fix bash issues with global vs local derefencing, and added optional directory creation

--- scripts/driver.sh
+++ scripts/driver.sh
@@ -6,10 +6,10 @@ for f in $1/*/*.pcap; do editcap -D 3 $f ${f%.pcap}-edited.pcap; done
 for f in $1/*/*-edited.pcap; do tshark -Y "ip.dsfield == 0x00" -r $f > ${f%.pcap}-leaky.txt; done
 for f in $1/*/*-edited.pcap; do tshark -Y "ip.dsfield == 0x80" -r $f > ${f%.pcap}-high.txt; done
 dest=$1
-find $dest -type f | rename 's:(^|.*/)([^/]*)/([^/]*)$:${dest}/$2_$3:'
+find $dest -type f | rename 's:(^|.*/)([^/]*)/([^/]*)$:'"${dest}"'/$2_$3:'
 for f in $dest/*.txt; do mv $f "${f/uc-berkeley-test-scenario-/}"; done
 cd ../
 arr=(pcaps/$dest/*-high.txt)
 for i in $(seq 0 ${#arr[@]}); do arr[$i]=${arr[$i]%high.txt}; done
-mkdir images_$dest
-for f in "${arr[@]}"; do python pcap2info.py 1 images_$dest ${f}leaky.txt ${f}high.txt; done
+mkdir -p images/images_$dest
+for f in "${arr[@]}"; do python pcap2info.py 1 images/images_$dest ${f}leaky.txt ${f}high.txt; done
